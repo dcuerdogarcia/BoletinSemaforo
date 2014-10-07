c585b00ece138aec520d29b1bd116421cdbb0ab9
dcuerdogarcia <dcuerdogarcia@eniac10.danielcastelao.org>
2014-10-07 10:06:23 +0200

    Commit inicial, usando Git

---
+++ src/boletin2/pkg7/Boletin27.java
@@ -0,0 +1,25 @@
+
+package boletin2.pkg7;
+
+import java.util.Scanner;
+
+public class Boletin27 {
+
+    
+    
+    public static void main(String[] args) {
+        float n, i, t;
+        System.out.println("Calculo de renta mensual\nIntroducir datos:\n\t-Nomina");
+        Scanner dato=new Scanner(System.in);
+        n=dato.nextFloat();
+        System.out.println("\t-interes anual en %:");
+        i=dato.nextFloat();
+        System.out.println("Introducir numero de años por el que se concede el prestamo:");
+        t=dato.nextFloat();
+        System.out.println("Renta mensual de "+((n*(i/100)*t)/1200));
+        
+        
+        
+    }
+    
+}
